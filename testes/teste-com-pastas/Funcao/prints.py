5104e7cd7387e826d3b90293803b08cd323c5217
Grégori Sabel <seriosabel57@gmail.com>
2022-05-24 13:29:11 -0300

    teste com pastas

---
+++ testes/teste-com-pastas/Funcao/prints.py
@@ -0,0 +1,4 @@
+
+
+def printandos():
+  print('printando coisa ')
---
+++ testes/teste-com-pastas/main.py
@@ -0,0 +1,15 @@
+
+# Assim que faz pra importar arquivos de pastas:
+
+from Funcao import prints 
+
+prints.printandos()
+
+
+# A pasta que contem os arquivos precisa também ter um arquivo chamado __init__.py (são 2 underlines no começo e 2 no final)
+# O python usa isso pra saber quais pastas procurar arquivos com código.
+# o arquivo __init__.py em si, geralmente estará vazio
+
+# nesse exemplo, eu criei uma pasta "Funcao", dentro criei um arquivo "prints.py" com as funções que eu queria
+# e também coloquei um __init__py vazio.
+# então só importei "from Funcao import prints"
